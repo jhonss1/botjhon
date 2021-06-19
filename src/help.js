ee14fae137e3334448c0f85e6e05b5e00bf8ed41
jhonss1 <86157238+jhonss1@users.noreply.github.com>
2021-06-19 10:58:58 -0300

    Add files via upload

---
+++ src/help.js
@@ -0,0 +1,106 @@
+
+const help = (prefix) => { 
+	return `                 
+
+CREATED BY:ՏᎡ.ᏦᎬᏦᎬ ͲᎻᎬ ᏴᎬՏͲ❦ᵃᵗᵉⁿᵃ© 2021
+REMODELADO POR: EU__JHON©
+
+╔══════════════════════
+║      MENU DE COMANDOS     
+║      (VERSÃO SIMPLES)      
+ ══════════════════════
+
+╭─────────────────────
+├ ᎬХᏟᏞႮՏᏆᏙϴ ᏢᎪᎡᎪ ᎪᎠᎷ's:
+ ─────────────────────
+
+COMANDO: *${prefix}kick @menciona o membro*
+AÇÃO: Remove um membro 
+
+COMANDO: *${prefix}add + número com cod do país e DDD*
+AÇÃO: Adiciona um novo membro
+
+COMANDO: *${prefix}listadmin*
+AÇÃO: Lista todos os ADM's do grupo
+
+COMANDO: *${prefix}welcome [1/0]*
+AÇÃO: Ativa/desativa a função de boas vindas
+
+COMANDO: *${prefix}grp fechar*
+AÇÃO: Priva o grp para ADM's
+
+COMANDO: *${prefix}grp abrir*
+AÇÃO: Abre o grp para membros.
+
+COMANDO: *${prefix}demote @adm*
+AÇÃO: Retira a função ADM de alguém
+
+COMANDO: *${prefix}promote @membro*
+AÇÃO: Adiciona q função de ADM à algum membro
+
+COMANDO: *${prefix}setname + novo nome*
+AÇÃO: Altera o nome do grp
+
+COMANDO: *${prefix}setdesc + nova desc*
+AÇÃO: Altera descrição do grp
+═══════════════════════════════════
+╭─────────────────────
+├   ᎷᏆᎠᏆᎪ & ՏͲᏆᏟᏦᎬᎡ:
+ ─────────────────────
+
+COMANDO: *${prefix}sticker + foto/Gif*
+AÇÃO: Faz uma fig do gif/fto desejado
+
+COMANDO: *{prefix}text3d + seu txt*
+AÇÃO: Cria uma foto cm seu txt
+
+COMANDO: *${prefix}ocr (marque uma foto)*
+AÇÃO: Pega os txts de uma foto
+
+
+COMANDO: *${prefix}tts pt seu txt*
+AÇÃO: Transforma txt em áudio
+
+COMANDO: *${prefix}toimg (marca uma fig)*
+AÇÃO: Converte fig em foto
+
+COMANDO: *${prefix}phlogo seu|txt*
+AÇÃO: Cria logo do porn hub cm seu txt
+
+COMANDO: *${prefix}creator*
+AÇÃO: Informa o número do criador
+═══════════════════════════════════
+╭──────────────────────
+├ EXCLUSIVO ᏢARA O ᏟᎡᏆᎪᎠϴᎡ
+ ──────────────────────
+
+COMANDO: *${prefix}clearall*
+AÇÃO: Limpa tdo o whats do bot
+
+COMANDO: *${prefix}block*
+AÇÃO: *Bloqueia um número
+
+COMANDO: *${prefix}unblock*
+AÇÃO: Desbloqueia um número
+
+COMANDO: *${prefix}leave*
+AÇÃO: Desconhecida
+
+COMANDO: *${prefix}setpp*
+AÇÃO: Desconhecida
+
+COMANDO: *${prefix}setprefix*
+AÇÃO: Troca a sigla de comandos
+═══════════════════════════════════
+
+VERSÃO ORIGINAL: ՏᎡ.ᏦᎬᏦᎬ ͲᎻᎬ ᏴᎬՏͲ❦ᵃᵗᵉⁿᵃ© 2021
+VERSÃO REMODELADA: EU__JHON©
+
+
+`
+}
+exports.help = help
+
+
+
+  
